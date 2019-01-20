8e0597fc7968fdf460124e2d67186848e35d0191
Simon Kateregga <spkateregga@gmail.com>
2019-01-19 23:59:50 -0600

    A project should have an owner.

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -5,6 +5,9 @@ use Faker\Generator as Faker;
 $factory->define(App\Project::class, function (Faker $faker) {
     return [
         'title' => $this->faker->sentence,
-        'description' => $this->faker->paragraph
+        'description' => $this->faker->paragraph,
+        'owner_id' => function() {
+          return factory(App\User::class)->create()->id
+        }
     ];
 });
