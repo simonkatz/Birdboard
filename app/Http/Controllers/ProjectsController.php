779f0dd5c3d993d7dc51cf1d412e8b1cb8498516
Simon Kateregga <spkateregga@gmail.com>
2019-01-19 23:37:46 -0600

    Add project.show test.

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -7,19 +7,29 @@ use App\Project;
 
 class ProjectsController extends Controller
 {
-    public function store()
-    {
-        $attributes = request()->validate(['title' => 'required', 'description' => 'required']);
-        
-        Project::create($attributes);
-        
-        return redirect('/projects');
-    }
-    
+  
     public function index()
     {
         $projects = Project::all();
         
         return view('projects.index', compact('projects'));
     }
+    
+    public function show(Project $project)
+    {    
+        return view('projects.show', compact('project'));
+    }
+    
+    public function store()
+    {
+        $attributes = request()->validate([
+          'title' => 'required', 
+          'description' => 'required'
+        ]);
+        
+        Project::create($attributes);
+        
+        return redirect('/projects');
+    }
+    
 }
