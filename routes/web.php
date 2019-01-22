22cb213338456cc973b43adfe414887c5fa4d895
Simon Kateregga <spkateregga@gmail.com>
2019-01-22 14:47:55 -0600

    Add authentication.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/projects', 'ProjectsController@index');
 Route::get('/projects/{project}', 'ProjectsController@show');
 Route::post('/projects', 'ProjectsController@store')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
